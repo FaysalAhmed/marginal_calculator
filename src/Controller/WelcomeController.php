5276304a77dd1bf81b6a71f354e13d39a4bb3921
Faysal Ahmed <faysal.ahmed833@gmail.com>
2020-04-12 21:44:14 +0600

    basic welcome page

---
+++ src/Controller/WelcomeController.php
@@ -0,0 +1,24 @@
+<?php 
+// welcome controller 
+// entry point of the project 
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class WelcomeController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function index()
+    {
+        // show the welcome page
+        // we will have a profit history. we will fetch latest profit from DB.
+        //
+        $profit = 0;
+       return $this->render('welcome/index.html.twig');
+    }
+} 
